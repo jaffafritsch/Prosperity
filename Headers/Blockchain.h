66c2dcdc7875424878446426702d96ad6df8fd53
Jaffafritsch <jaffafritsch@gmail.com>
2025-07-14 13:50:51 +1000

    Update

---
+++ Headers/Blockchain.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <vector>
+#include <unordered_map>
+#include "BlockchainTypes.h"
+
+class Blockchain {
+private:
+    std::vector<Block> chain;  // Blockchain consisting of blocks
+    std::vector<Transaction> pendingTransactions;  // Transactions waiting to be added to a block
+    std::vector<Contract> pendingContracts;  // Contracts waiting to be added to a block
+    std::unordered_map<std::string, RSA*> publicKeyMap;  // Map of wallet IDs to their public keys
+
+public:
+    // Constructor to initialize blockchain with genesis block
+    Blockchain();
+
+    // Method to add a new transaction to pendingTransactions
+    void createTransaction(Transaction transaction);
+
+    // Method to add a new contract to pendingContracts
+    void createContract(Contract contract);
+
+    // Method to mine pending transactions and add a new block to the blockchain
+    void minePendingTransactions();
+
+    // Method to validate if a block's hash is correct
+    bool isBlockHashValid(const Block& block);
+
+    // Method to validate if a transaction is valid
+    bool isTransactionValid(const Transaction& tx);
+
+    // Method to validate the integrity of the entire blockchain
+    bool isChainValid();
+
+    // Method to print the details of all blocks in the blockchain
+    void printChain();
+
+    // Method to notify all wallets about the state of the blockchain
+    void notifyWallets(std::vector<Wallet*>& wallets);
+};
--- Headers/BlockchainTypes.h
+++ Headers/BlockchainTypes.h
@@ -1,6 +1,40 @@
-//////////////////
-// Transactions //
-//////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+// Wallet ///////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+#ifndef WALLET_H
+#define WALLET_H
+
+#include <string>
+#include <vector>
+#include <openssl/rsa.h>
+
+class Wallet {
+public:
+    Wallet(std::string id); // Constructor with wallet ID
+    ~Wallet();  // Destructor to free RSA keys
+
+    // Method to send funds to another wallet
+    Transaction sendFunds(Wallet& receiver, float amount);
+
+    // Method to update the balance of this wallet based on blockchain transactions
+    void updateBalance(const std::vector<Transaction>& transactions);
+
+    // Method to print wallet details
+    void printWalletData() const;
+
+    std::string id; // Wallet ID
+    float balance; // Wallet balance
+    RSA* publicKey; // Public key of the wallet for verification
+
+private:
+    RSA* privateKey;  // Private key for signing transactions
+    void generateKeys();  // Method to generate RSA key pair
+};
+
+#endif // WALLET_H
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Transaction //////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
 #ifndef TRANSACTION_H
 #define TRANSACTION_H
 
@@ -30,9 +64,9 @@ public:
 };
 
 #endif // TRANSACTION_H
-///////////////
-// Contracts //
-///////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+// Contract //////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////
 #ifndef CONTRACT_H
 #define CONTRACT_H
 
@@ -67,46 +101,12 @@ public:
 };
 
 #endif // CONTRACT_H
-/////////////
-// Wallets //
-/////////////
-#ifndef WALLET_H
-#define WALLET_H
-
-#include <string>
-#include <vector>
-#include <openssl/rsa.h>
-#include "Transaction.h"
-
-class Wallet {
-public:
-    Wallet(std::string id); // Constructor with wallet ID
-    ~Wallet();  // Destructor to free RSA keys
-
-    // Method to send funds to another wallet
-    Transaction sendFunds(Wallet& receiver, float amount);
-
-    // Method to update the balance of this wallet based on blockchain transactions
-    void updateBalance(const std::vector<Transaction>& transactions);
-
-    // Method to print wallet details
-    void printWalletData() const;
-
-    std::string id; // Wallet ID
-    float balance; // Wallet balance
-    RSA* publicKey; // Public key of the wallet for verification
-
-private:
-    RSA* privateKey;  // Private key for signing transactions
-    void generateKeys();  // Method to generate RSA key pair
-};
-
-#endif // WALLET_H
-///////////
-// Block //
-///////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Block //////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
 #ifndef BLOCK_H
 #define BLOCK_H
+
 #pragma once
 #include <string>
 #include <vector>
